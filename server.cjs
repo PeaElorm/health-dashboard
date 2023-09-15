99221e955db9c5076f0bd2e6ffa678f1e9b68a6e
https://github.com/PeaElorm/test_demo.git <elormavugla7@gmail.com>
2023-09-15 09:02:05 +0000

    starter, login, and local api

---
+++ server.cjs
@@ -0,0 +1,16 @@
+const express = import("express");
+// Add cors as a middleware
+const cors = import("cors");
+const app = express();
+
+app.use(cors());
+
+app.use("/login", (req, res) => {
+  res.send({
+    token: "testing123",
+  });
+});
+
+app.listen(8080, () =>
+  console.log("API is running on http://localhost:8080/login")
+);
